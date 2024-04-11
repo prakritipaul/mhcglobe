531a602095287ef5c0ff3a5b4552e7aa1123bd12
Prakriti Chacha <pprakriti16@gmail.com>
2024-04-11 17:58:17 -0400

    updated parameters with model_compiler_params and mhcglobe_callbacks

--- src/parameters.py
+++ src/parameters.py
@@ -12,3 +12,14 @@ optimizer_params = {1: {"optimizer_type": "RMSprop",
                             "momentum": 0.5,
                             "epsilon": 3.17051703095139e-07,
                             "centered": True}}
+
+model_compiler_params = {"optimizer": "RMSprop", 
+                           "loss": "inequality_loss.MSEWithInequalities().loss",
+                           "metrics": ["mean_absolute_error", "mean_squared_error", "root_mean_squared_error"]}
+
+mhcglobe_callbacks = [Callbacks.EarlyStopping(
+                monitor='val_loss',
+                patience=20,
+                mode='min',
+                baseline=1,
+                min_delta=0.0001)]
